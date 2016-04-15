699c8473f27f35c3e4e56240bf0fcce67a57f625
brian915 <brian@brianrunk.net>
2016-04-15 19:47:04 -0400

    added note re: "patron" role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  #consider adding a "patron" role, or super_guest, for donations and special treats 
   enum role: [:reader,:admin]
   after_initialize :set_default_role, :if => :new_record?
 
