0b23ef06e9354618a993635dbc230e0b7fd1a950
brian915 <brian@brianrunk.net>
2016-04-15 19:46:13 -0400

    changed name of "guest" to "reader"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
-  enum role: [:guest,:admin]
+  enum role: [:reader,:admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
-    self.role ||= :guest
+    self.role ||= :reader
   end
 
   # Include default devise modules. Others available are:
