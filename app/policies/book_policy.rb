2f7ac2eed1445eb14bab31d9bf47b9310672cd30
brian915 <brian@brianrunk.net>
2016-04-16 21:11:14 -0400

    added authorization policy for books

---
+++ app/policies/book_policy.rb
@@ -0,0 +1,38 @@
+class BookPolicy
+  attr_reader :current_user, :model
+
+  def initialize(current_user, model)
+    @current_user = current_user
+    @book = model
+  end
+
+  def index?
+    @current_user.admin?
+  end
+
+  def show?
+    @current_user.admin? or @current_user == @user
+  end
+
+  def new?
+    @current_user.admin?
+  end
+
+  def edit?
+    @current_user.admin?
+  end
+
+  def create?
+    @current_user.admin?
+  end
+
+  def update?
+    @current_user.admin?
+  end
+
+  def destroy?
+    return false if @current_user == @user
+    @current_user.admin?
+  end
+
+end
